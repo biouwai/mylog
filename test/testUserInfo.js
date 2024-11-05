b98d6b3e13d0cf8318823a821e4f6301c1b32b7e
biouwai <2019499916@qq.com>
2024-11-05 20:53:23 +0800

    feat: 用户创建单元测试

---
+++ test/testUserInfo.js
@@ -0,0 +1,21 @@
+/**
+ * @description 单元测试的用户信息
+ * @author 双越老师
+ */
+
+/**
+ * 【特别提醒】cookie 是用户的敏感信息，此处只能是**测试**用户的 cookie
+ * 每次测试用户重新登录，都需要更新这里的 cookie
+ */
+
+module.exports = {
+  Z_ID: 1,
+  Z_USER_NAME: "zhangsan",
+  Z_COOKIE:
+    "weibo.sid=AkqeMlGj2jvg7fn43e0hnDW2rbrdkcAY; weibo.sid.sig=RQXlEosIWf1xLovZWty6EU84l1M",
+
+  L_ID: 2,
+  L_USER_NAME: "lisi",
+  L_COOKIE:
+    "weibo.sid=686m0u92zMTCmmSQY85trYv_PScDgpFx; weibo.sid.sig=Za_Oj-wSLFrG5QzQhqQU0YQxOFo",
+};
